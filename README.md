609be7b77e36262f1caf034d862f3837670c4b70
David Lee <david.isaac.lee@gmail.com>
2017-06-08 14:31:06 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+## Recurse Center - Code for Art
+
+This repo contains starter code for making art (or at least doing visual things) with code, using [p5.js](https://p5js.org/).
+
+If you have git on your machine, clone this repo with:
+
+```git clone git@github.com:aac/rc-codeforart.git```
+
+The repo contains an HTML file, `index.html`, which contains a canvas element and includes the Javascript files for p5. It also loads `sketch.js`, which contains our code for interacting with p5 and drawing things in the canvas element. In the `empty` branch, `sketch.js` just contains an empty p5.js skeleton. The various branches have different sketches. List the branches with `git branch`.
+
+To see the output of your code, just load index.html in your web browser. (On macOS you can do that from the CLI by entering `open index.html` inside the git repo directory.)
+
+p5.js has a lot of awesome functionality that I won't cover exhaustively. Check out [the reference page](https://p5js.org/reference/) for the full list of included features.
+
+One of the best resources for beginners is Dan Shiffman's book [*The Nature Of Code*](http://natureofcode.com/book/) (featuring RC S1'17 Evan Emolo in the credits). If you're looking for ideas, it's a great place to start. The book uses the Java based Processing programming environment, but there are [p5.js versions of the examples](https://github.com/shiffman/The-Nature-of-Code-Examples-p5.js/).
+
+Dan also hosts a YouTube channel called [The Coding Train](http://thecodingtrain.com/) where he walks through a wide range of visual (and other) programming concepts.
---
+++ sketch.js
@@ -0,0 +1,325 @@
+// Range : { lower : Number, upper : Number }
+const Range = {
+	span: (range) => range.upper - range.lower,
+
+	// convert(3, { from: range1, to: range2 })
+	// convert(3, { to: range2, from: range1 })
+	convert: (value, options) =>
+		(value - options.from.lower) 
+			/ Range.span(options.from)
+			* Range.span(options.to)
+			+ options.to.lower,
+
+	clamp: (value, range) =>
+		Math.max(range.lower, Math.min(range.upper, value)),
+
+	normalize: (value, range, shouldClamp) => {
+		const willClamp = (shouldClamp === undefined)
+			? false
+			: shouldClamp;
+		const normalizedRange = { lower: 0, upper: 1 };
+		var retval = Range.convert(value, { 
+			from: range, 
+			to: normalizedRange
+		});
+
+		if (willClamp) {
+			retval = Range.clamp(retval, normalizedRange);
+		}
+
+		return retval;
+	}
+};
+
+
+const fps = 30;
+var timescale = 1;
+var isPaused = false;
+
+
+// -- Types -- //
+
+function State(options) {
+	const defaultOptions = {
+		gridWidth: 8,
+		gridHeight: 6
+	};
+
+	const opts =
+		Object.assign({}, defaultOptions, options);
+
+	return Object.assign(this, {
+		grid: createGrid(opts.gridWidth, opts.gridHeight, chroma('black')),
+		stack: []
+	});
+}
+
+
+function Command(options) {
+	const defaultOptions = {
+		makeSource: function (grid, mod) {
+			return createGrid(grid.width, grid.height, chroma('black'));
+		},
+
+		transform: function (grid, mod) {
+			return grid;
+		}
+	}
+
+	const opts =
+		Object.assign({}, defaultOptions, options);
+
+	return Object.assign(this, {
+		makeSource: opts.makeSource,
+		transform: opts.transform
+	});
+}
+
+const colorCommand = new Command({
+	makeSource: function (grid, mod) {
+		var newGrid = cloneGrid(grid);
+
+		for (var x = 0; x < grid.width; x++) {
+			for (var y = 0; y < grid.height; y++) {
+				const hue = Range.convert(x, {
+					from: { lower: 0, upper: grid.width },
+					to: { lower: 0, upper: 360 }
+				});
+				newGrid.tiles[x][y].color = chroma.hsl(hue, 1, 0.5);
+			}
+		}
+
+		return newGrid;
+	},
+
+	transform: function (grid, mod) {
+		var newGrid = cloneGrid(grid);
+
+		for (var x = 0; x < grid.width; x++) {
+			for (var y = 0; y < grid.height; y++) {
+				const oldColor = grid.tiles[x][y].color;
+				const oldHue = oldColor.get('hsl.h');
+				const newHue = (oldHue + mod) % 360;
+
+				newGrid.tiles[x][y].color =
+					oldColor.set('hsl.h', newHue);
+			}
+		}
+
+		return newGrid;
+	},
+});
+
+const desyncCommand = new Command({
+	makeSource: function (grid, mod) {
+		var newGrid = cloneGrid(grid);
+
+		for (var x = 0; x < grid.width; x++) {
+			for (var y = 0; y < grid.height; y++) {
+				if (y % (mod + 2) == 0) {
+					newGrid.tiles[x][y].color = chroma('white');
+				}
+			}
+		}
+
+		return newGrid;
+	},
+
+	transform: function (grid, mod) {
+		var newGrid = cloneGrid(grid);
+
+		for (var x = 0; x < grid.width; x++) {
+			for (var y = 0; y < grid.height; y++) {
+				if ((y % 2) == 0) {
+					newGrid.tiles[x][y].color = grid.tiles[x][y].color;
+				} else {
+					const offset = -1;
+					const dstCoordinate = wrapGridCoordinate(x + offset, y, grid);
+
+					newGrid.tiles[dstCoordinate.x][dstCoordinate.y].color =
+						grid.tiles[x][y].color;
+				}
+			}
+		}
+
+		return newGrid;
+	},
+});
+
+
+const rotateCommand = new Command({
+	makeSource: function (grid, mod) {
+		var newGrid = cloneGrid(grid);
+
+		for (var x = 0; x < grid.width; x++) {
+			for (var y = 0; y < grid.height; y++) {
+				const xr = Range.convert(x, {
+					from: { lower: 0, upper: grid.width },
+					to: { lower: 0, upper: TWO_PI }
+				});
+
+				const yr = Range.convert(y, {
+					from: { lower: 0, upper: grid.height },
+					to: { lower: 0, upper: TWO_PI }
+				});
+
+				newGrid.tiles[x][y].color =
+					chroma.hsl(Math.cos(xr), Math.cos(yr), Math.cos(xr) - Math.sin(yr));
+			}
+		}
+
+		return newGrid;
+	},
+
+	transform: function (grid, mod) {
+		var newGrid = cloneGrid(grid);
+
+		for (var x = 0; x < grid.width; x++) {
+			for (var y = 0; y < grid.height; y++) {
+				const angle = 0.3;
+				const centeringOffset = {
+					x: -grid.width / 2,
+					y: -grid.height / 2
+				};
+				const rotationMatrix = [
+					[Math.cos(angle), -Math.sin(angle)],
+					[Math.sin(angle), Math.cos(angle)]
+				];
+
+				// Offset grid so that origin is in center of display.
+				const offsetX = x + centeringOffset.x;
+				const offsetY = y + centeringOffset.y;
+
+				// Rotate around center of display.
+				const rotatedPoint =
+					applyTransform(rotationMatrix, { x: offsetX, y: offsetY });
+
+				// Unoffset grid to bring back to original centering.
+				const unoffsetX = rotatedPoint.x - centeringOffset.x;
+				const unoffsetY = rotatedPoint.y - centeringOffset.y;
+
+				// Wrap coordinate to grid.
+				const dstCoordinate = wrapGridCoordinate(unoffsetX, unoffsetY, grid);
+
+				newGrid.tiles[dstCoordinate.x][dstCoordinate.y] =
+					grid.tiles[x][y];
+			}
+		}
+
+		return newGrid;
+	},
+});
+
+
+// -- Core -- //
+
+const commands = [
+	colorCommand,
+	desyncCommand,
+	rotateCommand
+];
+
+const charToCommandIndex = {
+	'a': 0,
+	's': 1,
+	'd': 2
+};
+
+var state = new State({ 
+	gridWidth: 8, 
+	gridHeight: 6
+});
+
+function setup() {
+	createCanvas(
+		windowWidth, 
+		windowHeight);
+	noLoop();
+}
+
+function draw() {
+	const dt = isPaused ? 0 : (timescale / fps);
+	state = tick(dt, state);
+
+	render(state);
+}
+
+function tick(dt, model) {
+	return model;
+}
+
+const tileSize = 40;
+function render(model) {
+	const renderedGrid = model.stack.reduce((grid, cmd, idx) => {
+		if (idx == 0) {
+			return cmd.makeSource(grid, idx);
+		} else {
+			return cmd.transform(grid, idx);
+		}
+	}, model.grid);
+
+	noStroke();
+	for (var x = 0; x < model.grid.width; x++) {
+		for (var y = 0; y < model.grid.height; y++) {
+			fill(renderedGrid.tiles[x][y].color.hex());
+			rect(x * tileSize, y * tileSize, tileSize, tileSize);
+		}		
+	}
+}
+
+// -- Events -- //
+
+function keyTyped() {
+	if (charToCommandIndex[key] != null) {
+		state.stack.push(commands[charToCommandIndex[key]]);
+		redraw();
+	} else if (keyCode == 13) {
+		// If "Enter" was typed...
+		state.stack = [];
+		redraw();
+	}
+}
+
+
+// -- Helpers -- //
+
+function createGrid(width, height, color) {
+	const createColor = typeof color === 'function'
+		? color
+		: () => color;
+
+	var tiles = [];
+	for (var x = 0; x < width; x++) {
+		tiles.push([]);
+		for (var y = 0; y < height; y++) {
+			tiles[x].push({ color: createColor(x, y) });
+		}
+	}
+
+	return {
+		width: width,
+		height: height,
+		tiles: tiles,
+	};
+}
+
+function cloneGrid(grid) {
+	return createGrid(
+		grid.width, 
+		grid.height,
+		(x, y) => grid.tiles[x][y].color);
+}
+
+function wrapGridCoordinate(x, y, grid) {
+	return {
+		x: floor(Math.abs(grid.width + x) % grid.width),
+		y: floor(Math.abs(grid.height + y) % grid.height),
+	};
+}
+
+function applyTransform(matrix, vector) {
+	return {
+		x: matrix[0][0] * vector.x + matrix[0][1] * vector.y,
+		y: matrix[1][0] * vector.x + matrix[1][1] * vector.y
+	};
+}
